60cba6920943b19db83eb3dbc05703fe9417c6df
prathibha <prathibhap.96@gmail.com>
2022-06-16 20:25:39 +0530

    modified product data

--- src/component/data.jsx
+++ src/component/data.jsx
@@ -1,29 +1,29 @@
 const data = {
   products: [
     {
-      name: "mythireyan",
-      category: "shirts",
+      name: "DNMX",
+      category: "Checked Shirt with Spread-Collar",
       image: "/images/image1.png",
       price: 120,
       stock: 10,
     },
     {
-      name: "prathibha",
-      category: "shirts",
+      name: "DNMX",
+      category: "Checked Shirt with Cut-away-Collar",
       image: "/images/image4.png",
       price: 220,
       stock: 10,
     },
     {
-      name: "sunshine",
-      category: "shirts",
+      name: "DNMX",
+      category: "Checked Shirt with Roll-Up Tabs",
       image: "/images/image3.png",
       price: 120,
       stock: 0,
     },
     {
-      name: "Moon",
-      category: "shirts",
+      name: "DNMX",
+      category: "Checked Shirt with Roll-Up Sleeve",
       image: "/images/image4.png",
       price: 350,
       stock: 20,
